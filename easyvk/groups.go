2ec148471ad623b846d57b79ac174f1cd588fcd5
Igor Ivaniuk <igorivaniuk@gmail.com>
2018-01-15 22:16:35 +0200

    feat: callback api configure

--- easyvk/groups.go
+++ easyvk/groups.go
@@ -2,6 +2,7 @@ package easyvk
 
 import (
 	"encoding/json"
+	"strconv"
 	"strings"
 )
 
@@ -14,10 +15,15 @@ type GroupsByIdResponse []GroupObject
 
 // GetById returns information about communities by their IDs.
 // https://vk.com/dev/groups.getById
-func (g *Groups) GetById(groupIds []string, fields string) (GroupsByIdResponse, error) {
+func (g *Groups) GetById(groupIds []int, fields string) (GroupsByIdResponse, error) {
+	var groupIdsStr []string
+	for _, gid := range groupIds {
+		text := strconv.Itoa(gid)
+		groupIdsStr = append(groupIdsStr, text)
+	}
 	params := map[string]string{
-		"group_ids": strings.Join(groupIds, ","),
-		"fields":   fields,
+		"group_ids": strings.Join(groupIdsStr, ","),
+		"fields":    fields,
 	}
 	resp, err := g.vk.Request("groups.getById", params)
 	if err != nil {
@@ -40,19 +46,94 @@ type GetCallbackConfirmationCodeResponse struct {
 
 // GetCallbackConfirmationCode returns Callback API confirmation code for the community.
 // https://vk.com/dev/groups.getCallbackConfirmationCode
-func (g *Groups) GetCallbackConfirmationCode(groupId string) (GetCallbackConfirmationCodeResponse, error) {
+func (g *Groups) GetCallbackConfirmationCode(groupId int) (*GetCallbackConfirmationCodeResponse, error) {
 	params := map[string]string{
-		"group_id": groupId,
+		"group_id": strconv.Itoa(groupId),
 	}
 	resp, err := g.vk.Request("groups.getCallbackConfirmationCode", params)
+
 	if err != nil {
-		return GetCallbackConfirmationCodeResponse{}, err
+		return nil, err
 	}
-	var codeRes GetCallbackConfirmationCodeResponse
-	err = json.Unmarshal(resp, &codeRes)
+	res := &GetCallbackConfirmationCodeResponse{}
+	err = json.Unmarshal(resp, res)
 	if err != nil {
-		return GetCallbackConfirmationCodeResponse{}, err
+		return nil, err
 	}
 
-	return codeRes, nil
+	return res, nil
+}
+
+// https://vk.com/dev/groups.getCallbackServers
+type GetCallbackServersResponse struct {
+	Count int              `json:"count"`
+	Items []CallbackServer `json:"items"`
+}
+
+type CallbackServer struct {
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	CreatorId string `json:"creator_id"`
+	Url       string `json:"url"`
+	SecretKey string `json:"secret_key"`
+	/*
+		unconfigured — адрес не задан;
+		fail — подтвердить адрес не удалось;
+		wait — адрес ожидает подтверждения;
+		ok — сервер подключен.
+	*/
+	Status string `json:"status"`
+}
+
+// https://vk.com/dev/groups.getCallbackServers
+func (g *Groups) GetCallbackServers(groupId int, serverIds []int) (*GetCallbackServersResponse, error) {
+	var serverIdsStr []string
+	for _, gid := range serverIds {
+		text := strconv.Itoa(gid)
+		serverIdsStr = append(serverIdsStr, text)
+	}
+	params := map[string]string{
+		"group_id":   strconv.Itoa(groupId),
+		"server_ids": strings.Join(serverIdsStr, ","),
+	}
+	resp, err := g.vk.Request("groups.getCallbackServers", params)
+
+	if err != nil {
+		return nil, err
+	}
+	res := &GetCallbackServersResponse{}
+	err = json.Unmarshal(resp, res)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+// https://vk.com/dev/groups.addCallbackServer
+type AddCallbackServerResponse struct {
+	ServerId int `json:"server_id"`
+}
+
+// https://vk.com/dev/groups.addCallbackServer
+func (g *Groups) AddCallbackServer(groupId int, url, title, secretKey string) (*AddCallbackServerResponse, error) {
+	params := map[string]string{
+		"group_id":   strconv.Itoa(groupId),
+		"url":        url,
+		"title":      title,
+		"secret_key": secretKey,
+	}
+	resp, err := g.vk.Request("groups.addCallbackServer", params)
+
+	if err != nil {
+		return nil, err
+	}
+
+	res := &AddCallbackServerResponse{}
+	err = json.Unmarshal(resp, res)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
 }
